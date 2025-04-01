c2d7d696133cd9da4990c989066a93add25564e4
Artem Stepanenko <alexplay.stepanenko@gmail.com>
2025-04-01 12:44:23 +0300

    feat: player fsm base logic

--- Assets/Scripts/Player/Controls/MovementSystem.cs
+++ Assets/Scripts/Player/Controls/MovementSystem.cs
@@ -1,7 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+using Utiles.FSM;
+
 namespace Player.Controls
 {
-    public class MovementSystem
+    public class MovementSystem : MonoBehaviour
     {
-        
+        public bool IsMovementDone { get; private set; }
+
+        [SerializeField] private NavMeshAgent _agent;
+
+        public void SetDestination(Vector3 point)
+        {
+            if (_agent is null)
+            {
+                Debug.LogError($"Agent for {this} system doesnt initialized");
+
+                return;
+            }
+
+            _agent.SetDestination(point);
+        }
     }
 }
--- Assets/Scripts/Player/FSM/States/PlayerIdleState.cs
+++ Assets/Scripts/Player/FSM/States/PlayerIdleState.cs
@@ -1,7 +1,27 @@
+using Utiles.FSM;
+
 namespace Player.FSM.States
 {
-    public class PlayerIdleState
+    public class PlayerIdleState : State
     {
+        public PlayerIdleState(StateType stateType)
+        {
+            StateType = stateType;
+        }
         
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            
+        }
     }
 }
