fd1c875fd46040026e8eaef80154979a095bd913
Artem Stepanenko <alexplay.stepanenko@gmail.com>
2025-07-08 12:13:53 +0300

    update: temp changes
    # Conflicts:
    #	Assets/Scripts/UI/PopUp/Variants.meta

--- Assets/Scripts/UI/Windows/Variants/SeedsWindow/SeedsWindow.cs
+++ Assets/Scripts/UI/Windows/Variants/SeedsWindow/SeedsWindow.cs
@@ -1,9 +1,21 @@
+using UI.ElementCard.Slot;
 using UI.Windows;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.PopUp.Variants.Garden.Windows
 {
     public class SeedsWindow : AbstractWindow
     {
+        [Header("Seeds elements")]
+        [SerializeField] private DropSlot _seedDropSlot;
+
+        [Space, SerializeField] private Button _growButton;
+
+        [Header("Growing elements")] 
+        [SerializeField] private Slider _growingProgressSlider;
+
+        [Space, SerializeField] private Button _peakButton;
         
         
     }
