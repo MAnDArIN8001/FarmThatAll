4e6516ab6950dd385af25aaa575300fcbf310ee5
Modeus <blastachanel@gmail.com>
2025-07-14 01:54:14 +0300

    feat: recipe setup and converter logic

---
+++ Assets/Scripts/Building/Converter/Recipe.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Storage.Items;
+using UnityEngine;
+
+namespace Building.Converter
+{
+    [CreateAssetMenu(menuName = "Buildings/NewConverterRecipe")]
+    public class Recipe : ScriptableObject
+    {
+        [field: SerializeField] public string RecipeName { get; private set; }
+
+        [field: SerializeField] public int RecipeDurationMilliseconds { get; private set; } = 5000;
+        
+        [field: SerializeField] public ItemType RecipeOutputType { get; private set; }
+        [field: SerializeField] public int RecipeOutputAmount { get; private set; } = 1;
+        
+        [SerializeField] private List<Ingredient> ingredients;
+        
+        public IReadOnlyList<Ingredient> Ingredients => ingredients;
+    }
+
+    [Serializable]
+    public class Ingredient
+    {
+        [field: SerializeField] public ItemType Type { get; private set; }
+        
+        [field: SerializeField] public int Amount { get; private set; }
+    }
+}
