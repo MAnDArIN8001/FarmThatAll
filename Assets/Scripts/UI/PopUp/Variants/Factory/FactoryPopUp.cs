f2438711bc9c0f2880b828c7abaa571179beecfe
geeztd <vlad.kletsky@yandex.by>
2025-07-16 04:43:03 +0300

    popUp

--- Assets/Scripts/UI/PopUp/Variants/Factory/FactoryPopUp.cs
+++ Assets/Scripts/UI/PopUp/Variants/Factory/FactoryPopUp.cs
@@ -22,6 +22,15 @@ namespace UI.PopUp
         [SerializeField] private Image itemImage;
         [SerializeField] private Image progressBarImage;
 
+        [SerializeField] private Button _openDestroyWindowButton;
+        [SerializeField] private Button _closeDestroyWindowButton;
+
+        [SerializeField] private DestroyWindow _destroyWindow;
+
+        [SerializeField] private GameObject _tab;
+
+
+
         public FactoryBuilding factory;
 
         private int _localStorageCount;
@@ -50,6 +59,17 @@ namespace UI.PopUp
             
             collectButton.onClick.AddListener(CollectSteel);
             itemName.text = factory.GeneratedItemType.ToString();
+
+            if (_openDestroyWindowButton is not null)
+            {
+                _openDestroyWindowButton.onClick.AddListener(OpenDestroyWindow);
+            }
+
+            if (_closeDestroyWindowButton is not null)
+            {
+                _closeDestroyWindowButton.onClick.AddListener(CloseDestroyWindow);
+            }
+
         }
 
         private void OnDisable()
@@ -74,6 +94,37 @@ namespace UI.PopUp
         private void CollectSteel()
         {
             _playerStorage.IncreaseItem(factory.GeneratedItemType, factory.Get());
+        }
+
+        private void OpenDestroyWindow()
+        {
+            _destroyWindow.Open();
+
+            Vector3 targetPosition = _openDestroyWindowButton.transform.position;
+            StartCoroutine(MoveOverTime(_tab.transform, targetPosition, 0.5f));
+        }
+
+        public void CloseDestroyWindow()
+        {
+            _destroyWindow.Close();
+
+            Vector3 targetPosition = _closeDestroyWindowButton.transform.position;
+            StartCoroutine(MoveOverTime(_tab.transform, targetPosition, 0.5f));
+        }
+
+        private System.Collections.IEnumerator MoveOverTime(Transform transform, Vector3 target, float time)
+        {
+            Vector3 start = transform.position;
+            float elapsed = 0f;
+
+            while (elapsed < time)
+            {
+                transform.position = Vector3.Lerp(start, target, elapsed / time);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = target;
         }
     }
 }
